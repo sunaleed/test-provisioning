778edef953e3d93e914b61d5927c16a0eb2ed06e
sunaleed <kksk1441@gmail.com>
2025-07-27 12:53:56 +0100

    Update s3 resource name

--- terraform/s3/suna-id/suna_service_deployment.tf
+++ terraform/s3/suna-id/suna_service_deployment.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "service_deployment" {
-  bucket = "service-deployment"
+  bucket = "suna-service-deployment"
 }
 
 resource "aws_s3_bucket_accelerate_configuration" "service_deploymentaccelerate_configuration" {
--- terraform/s3/zerone-id/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_region" {
-  description = "aws region"
-  default     = "ap-northeast-2"
-}
