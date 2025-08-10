040d60f6943aeb822cfe46cecdfbd2f7957170b8
sunaleed <kksk1441@gmail.com>
2025-08-10 13:31:19 +0100

    Add terraform state

---
+++ terraform/init/suna-id/init.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "suna-id-apnortheast2-tfstate"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform-lock-table"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
